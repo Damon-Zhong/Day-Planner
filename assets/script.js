01ad2f3aeb459496d2b3748749f6e8c2f0a3ba24
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-05-29 14:49:32 -0400

    Bug fixing

--- assets/script.js
+++ assets/script.js
@@ -9,53 +9,55 @@ let Agenda = localStorage.Agenda ? JSON.parse(localStorage.Agenda) :
               {'Hour':'4PM', 'Schedule':''},
               {'Hour':'5PM', 'Schedule':''}];
 const agendaEl = document.querySelector("#Agenda");
+const currentTime = document.querySelector("#currentDay");
 
-    function renderList(){
-        agendaEl.innerHTML = '';
-
-
-        Agenda.forEach( 
-            function( item, index ){
-                agendaEl.innerHTML += `<div class="row  time-block" >
-                <div class="hour" id="${Agenda[index].Hour}">
-                    ${item.Hour}
-                </div>
-                <textarea id="${index}" class="${getTime(item)}  description"  onfocus="this.select()">${item.Schedule}</textarea> 
-                <div class="saveBtn">
-                    <i class="far fa-save" onClick='saveAgenda(event)'></i>
-                </div>
-              </div>`;
-            }
-        )
-    }
-    renderList();
-
-    function getTime(item){
-        let currentTime = moment().format('kk');    // get current hour
-        console.log(currentTime);
-        let scheduleTime = Number(item.Hour.slice(0,-2));// Numeric time on agenda
-        let scheduleTimeAMPM = item.Hour.substr(-2);// AM/PM of time on agenda
-        let convertedTime;
-        if(scheduleTimeAMPM == "AM"){
-            convertedTime = scheduleTime;
-        }else{
-            convertedTime = scheduleTime+12;
+function showSchedule(){
+    agendaEl.innerHTML = '';
+    Agenda.forEach( 
+        function( item, index ){
+            agendaEl.innerHTML += `<div class="row  time-block" >
+            <div class="hour">
+                ${item.Hour}
+            </div>
+            <textarea id="${index}" class="${getTime(item)}  description"  onfocus="this.select()">${item.Schedule}</textarea> 
+            <div class="saveBtn">
+                <i class="far fa-save" onClick='saveAgenda(event)'></i>
+            </div>
+            </div>`;
         }
+    )
+}
+showSchedule();
 
-        if(convertedTime<currentTime) return 'past';
-        else if(convertedTime==currentTime) return 'present';
-        else if(convertedTime>currentTime) return 'future';
+function getTime(item){
+    let currentTime = moment().format('kk');    // get current hour
+    console.log(currentTime);
+    let scheduleTime = Number(item.Hour.slice(0,-2));// Numeric time on agenda
+    let scheduleTimeAMPM = item.Hour.substr(-2);// AM/PM of time on agenda
+    let convertedTime;
+    //convert time-block to 24-hours format
+    if(scheduleTimeAMPM == "AM"){
+        convertedTime = scheduleTime;
+    }else{
+        convertedTime = scheduleTime+12;
     }
+    //reutrn class name based on currentTime
+    if(convertedTime<currentTime) return 'past';
+    else if(convertedTime==currentTime) return 'present';
+    else if(convertedTime>currentTime) return 'future';
+}
 
-    function saveAgenda(event){
-        event.preventDefault();
-        let textArea = event.target.parentElement.childNodes[3];
-        let idx = textArea.id;
-        // save input to Agenda
-        Agenda[idx].Schedule = textArea.value;
-        // save to local storage
-        localStorage.Agenda = JSON.stringify(Agenda);
+function saveAgenda(event){
+    event.preventDefault();
+    let textArea = event.target.parentElement.parentElement.childNodes[3];
+    let idx = textArea.id;
+    // save input to Agenda
+    Agenda[idx].Schedule = textArea.value;
+    // save to local storage
+    localStorage.Agenda = JSON.stringify(Agenda);
 
-        renderList();
-    }
+    showSchedule();
+}
 
+    //Show current time when page loaded
+    currentTime.textContent = setInterval(function(){currentTime.textContent=moment();}, 1000);
